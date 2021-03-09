03cac7ea28ded57454b5fd5dee5edec00957c1dd
ahmed tantawy <a7mad_gamal@live.com>
2021-03-09 17:11:28 +0200

    Initial Commit

---
+++ Healthy Sleep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Healthy Sleep
+//
+//  Created by Ahmed Tantawy on 09/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
